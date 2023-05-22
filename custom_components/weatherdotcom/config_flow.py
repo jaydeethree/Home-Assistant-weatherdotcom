a47c4b86b31a4f4e8053c6d5dea5da6093821510
jaydeethree <34945248+jaydeethree@users.noreply.github.com>
2023-05-22 15:29:06 -0700

    Fix undefined variables

--- custom_components/weatherdotcom/config_flow.py
+++ custom_components/weatherdotcom/config_flow.py
@@ -40,6 +40,8 @@ class WeatherFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         session = async_create_clientsession(self.hass)
 
         api_key = user_input[CONF_API_KEY]
+        latitude = user_input[CONF_LATITUDE]
+        longitude = user_input[CONF_LONGITUDE]
         headers = {
             'Accept-Encoding': 'gzip',
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
